f9cbbb6290dd7b1792fb6b26af6ec7a925196a02
Manu Datta <>
2021-11-29 16:45:56 +0000

    Added a Change Variable which stops the frame from reprinting if there is no change

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from pynput import keyboard
 from time import sleep
 from random import randint
 
+xlength = 45
+ylength = 12
+
 thenumber = 9
 rightline = 4
 
@@ -14,42 +17,63 @@ def IsItAWall(xSpot, ySpot):
     if xwalls[wallX] == xSpot:
       if ySpot == ywalls[wallX]:
         return True
-  if ySpot >= 10:
+  if ySpot >= ylength:
+    return True
+  elif ySpot <= 0:
+    return True
+  elif xSpot <= 0:
+    return True
+  elif xSpot >= xlength:
     return True
   return(False)
 
 def on_press(key):
   global rightline
   global thenumber
+  #If nothing changes the "frame" is not re-printed
+  #this reduces the screen glitches
+  change = False
+  jumping = False
+  #Checks What Key is pressed
   if key == keyboard.Key.left:
     thenumber = thenumber - 1
+    change = True
     if IsItAWall(thenumber, rightline):
       thenumber = thenumber + 1
+      change = False
   elif key == keyboard.Key.right:
     thenumber = thenumber + 1
+    change = True
     if IsItAWall(thenumber, rightline):
       thenumber = thenumber - 1
+      change = False
   elif key == keyboard.Key.up:
     rightline = rightline - 1
+    change = True
     if IsItAWall(thenumber, rightline):
       rightline = rightline + 1
+      change = False
+    jumping = True
   elif key == keyboard.Key.down:
     rightline = rightline + 1
+    change = True
     if IsItAWall(thenumber, rightline):
       rightline = rightline - 1
-  if not IsItAWall(thenumber, (rightline + 1)):
+      change = False
+  if not IsItAWall(thenumber, (rightline + 1)) and not jumping:
+    change = True
     rightline = rightline + 1
-  for line in range(1,10):
-    for block in range(0,40):
-      if block == thenumber and line == rightline:
-        print("O", end = '')
-      elif IsItAWall(block, line):
-        print("█", end = '')
-      else:
-        print("~", end = '')
-    print("")
-  for i in range(0,2):
-    print("") 
+  #Prints
+  if change:
+    for line in range(1,ylength):
+      for block in range(1,xlength):
+        if block == thenumber and line == rightline:
+          print("O", end = '')
+        elif IsItAWall(block, line):
+          print("█", end = '')
+        else:
+          print("~", end = '')
+      print("")
 
 def on_release(key):
     if key == keyboard.Key.esc:
